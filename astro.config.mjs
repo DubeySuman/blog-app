817ae724f4a989c621057645e47a08ad060670b1
DubeySuman <dubeysuman222@gmail.com>
2025-12-02 02:59:24 +0530

    Add Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [keystatic()],
+  adapter: vercel(),
   devToolbar: {
     enabled: false
   }
